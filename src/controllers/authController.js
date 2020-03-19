9d898d80ab595e0559f523752ba37512a9287494
thecurrentuser <thecurrentuser@live.com>
2020-03-19 09:55:18 -0300

    chore: Caso já exista um usuario com email cadastrado; remover pass do res

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,13 +6,19 @@ const router = express.Router();
 
 // Pegar parametros do usuario e repassar para user.Create
 router.post("/register", async (req, res) => {
-  try {
-    const user = await User.create(req.body);
+  // Adicionar uma validacao para aparecer uma mensagem mais "amigavel"
+  // Caso já exista um usuario com email cadastrado
 
+  try {
+    if (await user.findOne({ user }))
+      return res.status(400).send({ error: "user already exists" });
+    const user = await User.create(req.body);
+    //Apagar password assim que o usuario for criado para nao sair no res
+    user.password = undefined;
     return res.send({ user });
   } catch (error) {
     return res.status(400).send({ error: "registration failed" });
   }
 });
 
-module.exports = app => app.use('/auth', router)
+module.exports = app => app.use("/auth", router);
