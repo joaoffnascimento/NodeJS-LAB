5458c833fbc8bc838e71984de32928ed7b7e5b04
thecurrentuser <thecurrentuser@live.com>
2020-03-19 12:00:48 -0300

    feat: authMiddleware added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./controllers/authController")(app);
+require('./controllers/authController')(app);
+require('./controllers/projectController')(app);
 
 app.listen(3000);
