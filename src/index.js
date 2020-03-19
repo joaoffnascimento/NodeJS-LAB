b30336931268de47f02e1578b4dfaf2e4ed3eef2
thecurrentuser <thecurrentuser@live.com>
2020-03-19 09:01:40 -0300

    alteracoes na configuracao do servidor

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("OK");
-});
+require("./controllers/authController ")(app);
 
 app.listen(3000);
