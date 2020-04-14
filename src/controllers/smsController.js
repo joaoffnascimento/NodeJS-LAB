78d8e1b65a182ad40561b5de7ba9243d23738fe2
thecurrentuser <thecurrentuser@live.com>
2020-04-14 16:04:14 -0300

    twilio

---
+++ src/controllers/smsController.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('./../middlewares/auth');
+require('dotenv/config')
+const accountSid = "ACb2428312d4edd2fc80854babf89c980b"
+const authToken = "e406d3bad8bc542d3414f1b522e480af"
+const client = require('twilio')(accountSid, authToken)
+
+const MessagingResponse = require('twilio').twiml.MessagingResponse
+
+router.use(authMiddleware);
+
+router.post('/receive', async (req, res) => {
+    const twiml = new MessagingResponse()
+
+    try {
+        twiml.message = 'Histeria !!!'
+
+        res.writeHead(200, { 'Content-Type': 'text-xml' })
+        red.end(twiml.toString())
+
+        res.send(200)
+    } catch (error) {
+        return res.status(400).send({ error: error });
+    }
+
+})
+
+router.post('/:dest/:msg', async (req, res) => {
+    const { dest, msg } = req.params
+
+    try {
+        client.messages.create({
+            to: dest,
+            from: "+19094130543",
+            body: msg
+        }).then((message) => console.log('-->', message.sid))
+
+        res.send(200)
+
+    } catch (error) {
+        return res.status(400).send({ error: error });
+    }
+})
+
+module.exports = app => app.use('/sms', router);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./controllers/authController')(app);
 require('./controllers/projectController')(app);
+require('./controllers/smsController')(app);
 
 app.listen(3000);
